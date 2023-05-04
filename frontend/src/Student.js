a3363c7cec085444acb4d6141d3a184dfbcd9efe
razamehdisyed <razamehdisyed@gmail.com>
2023-05-04 10:43:35 +0500

    b

--- frontend/src/Student.js
+++ frontend/src/Student.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from "axios"
 
 const Student = () => {
-  return (
+
+    useEffect(()=>{
+        axios.get('localhost:8081/')
+        .then(res =>console.log(res))
+        .catch(err =>console.log(err))
+    },[])
+    return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-item-center'>
-        <div className='w-50 bg-white rounded'>
+        <div className='h-auto d-inline-block'>
             <button className='btn btn-success'>Add +</button>
             <table className='table'>
                 <thead>
