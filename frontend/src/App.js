a8a22f423cd37b8f61c9337ed129ee0ea82531cb
razamehdisyed <razamehdisyed@gmail.com>
2023-05-04 09:32:46 +0500

    server

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.js'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<Student />>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
